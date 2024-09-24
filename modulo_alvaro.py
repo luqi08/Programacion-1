78e21278e1c86326cc758db9f32fe35be791bb7e
Alvaro Beron <beronalvaro@gmail.com>
2024-09-24 17:14:11 -0300

    lo de antes con detalles

--- modulo_alvaro.py
+++ modulo_alvaro.py
@@ -1,4 +1,5 @@
-Boeing747 = [[0, 0, 0, 0, 0, 0 ,0], [0, 0, 0, 0, 0, 0, 0]]
+Boeing747 = [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+
 
 def verificarDisponibilidad(Boeing747):
     verificar = True
@@ -6,12 +7,19 @@ def verificarDisponibilidad(Boeing747):
         print(Boeing747)
     else:
         print("Este vuelo ya no cuenta con asientos disponibles")
+
+
 def mostrarAsientos():
     return
 
-pasajeros = {"dni":"Nombre Apellido"}
-pasajes = {000000:{"clase":"primera", "asiento":"G8", "dni":0000000}}
-aviones = {"Boeing747":Boeing747}
-vuelos = {"EZE":{"Fecha":"20/10/2024", "Origen":"Misiones", "avion":"Boeing747"}}
+
+pasajeros = {"dni": "Nombre Apellido"}
+pasajes = {000000: {"clase": "primera", "asiento": "G8", "dni": 0000000}}
+aviones = {"Boeing747": Boeing747}
+vuelos = {"EZE": {"Fecha": "20/10/2024", "Origen": "Misiones", "avion": "Boeing747"}}
 
 
+# Pasajeros: DNI, nombre, apellido
+# Pasajes: Numero de pasaje, numero de asiento, clase de asiento, codigo de equipaje
+# Aviones: Modelo, asientos
+# Vuelos: Destino, fecha, origen
