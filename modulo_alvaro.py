c3e5ad380ec0de177f85998037010b2afbf80e74
la-pochoclera <beronalvaro@gmail.com>
2024-09-24 16:25:48 -0300

    ejemplo diccionarios

---
+++ modulo_alvaro.py
@@ -0,0 +1,18 @@
+#Ejemplo de primeras funciones e idea del main
+Boeing747 = [[0, 0, 0, 0, 0, 0 ,0], [0, 0, 0, 0, 0, 0, 0]]
+
+def verificarDisponibilidad(Boeing747):
+    verificar = True
+    if verificar == True:
+        print(Boeing747)
+    else:
+        print("Este vuelo ya no cuenta con asientos disponibles")
+def mostrarAsientos():
+    return
+
+pasajeros = {"dni":"Nombre Apellido"}
+pasajes = {000000:{"clase":"primera", "asiento":"G8", "dni":0000000}}
+aviones = {"Boeing747":Boeing747}
+vuelos = {"EZE":{"Fecha":"20/10/2024", "Origen":"Misiones", "avion":"Boeing747"}}
+
+
