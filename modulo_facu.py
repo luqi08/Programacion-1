809fa378668e16c8a7621c5266234511c8755b32
FacundoMuruchi <facundomuruchi01@gmail.com>
2024-10-01 22:33:32 -0300

    verificacion de valores al cambiar asiento

--- modulo_facu.py
+++ modulo_facu.py
@@ -1,5 +1,5 @@
 #FUNCIONES
-"""FALTA FUNCION PARA CAMBIAR DE CLASE Y BUCLE PARA ERRORES DE TIPEO AL ELEGIR LETRA Y NUMERO DE ASIENTO"""
+"""FALTA FUNCION PARA CAMBIAR DE CLASE"""
 def crearMatriz(filas,columnas,relleno):
     return [[relleno] * columnas for fila in range(filas)]
 
@@ -21,13 +21,15 @@ def letraNumero(letra):
 def mostrarMatriz(matriz):
     longitud = len(matriz[0])
     ancho_columna = 2
-    
+    filas_totales = len(matriz)
+    medio = filas_totales // 2
+
     print("  " + " ".join(f"{i:>{ancho_columna}}" for i in range(1, longitud + 1)))
-    
+
     for indice, fila in enumerate(matriz):
         letra = chr(65 + indice)  # 65 es el código ASCII de 'A'
         print(f"{letra} " + " ".join(f"{el:>{ancho_columna}}" for el in fila))
-        if letra == 'C':
+        if indice == medio - 1:
             print('PASILLO'.center(longitud * 3 + 1, '='))
     print('PUNTA DEL AVION A LA IZQUIERDA - COLA DEL AVION A LA DERECHA')
 
@@ -36,27 +38,34 @@ def mostrarPasaje(codigoPasaje):
         print(f'{clave}: {valor}'.title())
 
 def cambiarAsiento(codigoPasaje):
-    filaAsiento = input('SELECCIONE LETRA (A,B,C,D,E,F): ')
-    filaColumna = input('SELECCIONE NUMERO (1,2,3,ETC.): ')
-    pasajes[codigoPasaje]['asiento'] = filaAsiento.capitalize() + filaColumna
+    longitud = len(matriz[0])
+    listaLetras = []
+    for indice, fila in enumerate(matriz):
+        letra = chr(65 + indice)
+        listaLetras.append(letra)
+    while True:
+        filaAsiento = input('SELECCIONE LETRA (A,B,C,ETC.): ')
+        if filaAsiento.capitalize() not in listaLetras:
+            print(f'{filaAsiento} NO ESTÁ EN EL RANGO')
+        else:
+            break
+    while True:
+        filaColumna = int(input('SELECCIONE NUMERO (1,2,3,ETC.): '))
+        if filaColumna < 1 or filaColumna > longitud:
+            print(f'{filaColumna} NO ESTÁ EN EL RANGO')
+        else:
+            break
+    pasajes[codigoPasaje]['asiento'] = filaAsiento.capitalize() + str(filaColumna)
     filaAsiento = letraNumero(filaAsiento.capitalize())
-    matriz[filaAsiento][int(filaColumna) - 1] = 1
+    matriz[filaAsiento][filaColumna - 1] = 1
 
 def modificarPasaje(pasajes):
     while True:
-        codigoPasaje = int(input('INGRESE EL CODIGO DEL PASAJE: '))
-        if codigoPasaje not in pasajes:
-            print(f'SU PASAJE {codigoPasaje} NO ESTA REGISTRADO')
-            while True:
-                print('[1] REINTENTAR')
-                print('[2] SALIR')
-                opcion = int(input('SELECCIONE UNA OPCION: '))
-                if opcion == 1:
-                    break
-                elif opcion == 2:
-                    exit()
-                else:
-                    print('INGRESE UN VALOR EN EL RANGO')
+        codigoPasaje = int(input('INGRESE EL CODIGO DEL PASAJE O [2] PARA SALIR: '))
+        if codigoPasaje == 2:
+            exit()
+        elif codigoPasaje not in pasajes:
+            print(f'SU PASAJE {codigoPasaje} NO ESTA REGISTRADO. REINTENTE...')
         else:
             print('--------------------------')
             mostrarPasaje(codigoPasaje)
@@ -67,10 +76,11 @@ def modificarPasaje(pasajes):
                 opcion = int(input('SELECCIONE UNA OPCION: '))
                 if opcion == 1:
                     print()
+                    print(f'ASIENTO ACTUAL: {pasajes[codigoPasaje]['asiento']}')
                     mostrarMatriz(matriz)
                     cambiarAsiento(codigoPasaje)
                     print()
-                    print('ASIENTO MODIFICADO CORRECTAMENTE')
+                    print(f'NUEVO ASIENTO: {pasajes[codigoPasaje]['asiento']}')
                     mostrarMatriz(matriz)
                     break
                 elif opcion == 2:
@@ -84,7 +94,5 @@ pasajes = {000000: {'avion': 4523452, "clase": "primera", "asiento": "G8", "pasa
 vuelos = {000000: {"Fecha": "20/10/2024", "Origen": "Misiones", 'destino': 'eze'}}
 aviones = {"modelo": 'Boeing747'}
 
-matriz = crearMatriz(6,30,0)
+matriz = crearMatriz(4,30,0)
 modificarPasaje(pasajes)
-print()
-mostrarPasaje(000000)
