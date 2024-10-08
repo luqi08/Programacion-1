d9cf4ca2151c6a5e6eecb92b5f21bbf0285ebadb
FacundoMuruchi <facundomuruchi01@gmail.com>
2024-10-08 20:29:26 -0300

    version vieja

--- modulo_facu.py
+++ modulo_facu.py
@@ -1,5 +1,5 @@
 #FUNCIONES
-"""FALTA FUNCION PARA CAMBIAR DE CLASE"""
+"ARREGLAR CREACION DE MATRICES AL USAR LA SEGUNDA OPCION DE CAMBIAR DE CLASE"
 def crearMatriz(filas,columnas,relleno):
     return [[relleno] * columnas for fila in range(filas)]
 
@@ -18,13 +18,16 @@ def letraNumero(letra):
         letra = 5
     return letra
 
-def mostrarMatriz(matriz):
+def mostrarMatriz(matriz, pasaje, codigo):
     longitud = len(matriz[0])
     ancho_columna = 2
     filas_totales = len(matriz)
     medio = filas_totales // 2
 
-    print("  " + " ".join(f"{i:>{ancho_columna}}" for i in range(1, longitud + 1)))
+    if esEjecutiva(pasaje, codigo):
+        print("  " + " ".join(f"{i:>{ancho_columna}}" for i in range(1, longitud + 1)))
+    else:
+        print("  " + " ".join(f"{i:>{ancho_columna}}" for i in range(5, longitud + 5)))
 
     for indice, fila in enumerate(matriz):
         letra = chr(65 + indice)  # 65 es el código ASCII de 'A'
@@ -37,7 +40,7 @@ def mostrarPasaje(codigoPasaje):
     for clave, valor in pasajes[codigoPasaje].items():
         print(f'{clave}: {valor}'.title())
 
-def cambiarAsiento(codigoPasaje):
+def cambiarAsiento(codigoPasaje, matriz):
     longitud = len(matriz[0])
     listaLetras = []
     for indice, fila in enumerate(matriz):
@@ -59,6 +62,14 @@ def cambiarAsiento(codigoPasaje):
     filaAsiento = letraNumero(filaAsiento.capitalize())
     matriz[filaAsiento][filaColumna - 1] = 1
 
+def esEjecutiva(pasajes, codigo):
+    asiento = pasajes[codigo]['asiento']
+    numero_asiento = int(asiento[1:])
+    if numero_asiento < 5:
+        return True
+    else:
+        return False
+
 def modificarPasaje(pasajes):
     while True:
         codigoPasaje = int(input('INGRESE EL CODIGO DEL PASAJE O [2] PARA SALIR: '))
@@ -71,28 +82,44 @@ def modificarPasaje(pasajes):
             mostrarPasaje(codigoPasaje)
             print('--------------------------')
             while True:
-                print('[1] CAMBIAR ASIENTO')
-                print('[2] SALIR')
+                print('[1] CAMBIAR ASIENTO DENTRO DE MISMA CLASE')
+                print('[2] CAMBIAR CLASE')
+                print('[3] SALIR')
                 opcion = int(input('SELECCIONE UNA OPCION: '))
                 if opcion == 1:
                     print()
-                    print(f'ASIENTO ACTUAL: {pasajes[codigoPasaje]['asiento']}')
-                    mostrarMatriz(matriz)
-                    cambiarAsiento(codigoPasaje)
+                    print(f'ASIENTO ACTUAL: {pasajes[codigoPasaje]['asiento']}, CLASE: {pasajes[codigoPasaje]['clase']}')
+                    if esEjecutiva(pasajes, codigoPasaje):
+                        mostrarMatriz(matrizEjecutiva, pasajes, codigoPasaje)
+                        cambiarAsiento(codigoPasaje, matrizEjecutiva)
+                    else:
+                        mostrarMatriz(matrizEconomica, pasajes, codigoPasaje)
+                        cambiarAsiento(codigoPasaje, matrizEconomica)
                     print()
-                    print(f'NUEVO ASIENTO: {pasajes[codigoPasaje]['asiento']}')
-                    mostrarMatriz(matriz)
+                    print(f'NUEVO ASIENTO: {pasajes[codigoPasaje]['asiento']}, CLASE: {pasajes[codigoPasaje]['clase']}')
                     break
                 elif opcion == 2:
+                    while True:
+                        print('[1] ECONÓMICA')
+                        print('[2] EJECUTIVA')
+                        opcion = int(input('SELECCIONE UNA OPCION: '))
+                        if opcion == 1:
+                            ...
+                        elif opcion == 2:
+                            ...
+                        else:
+                            print('INGRESE UN VALOR EN EL RANGO')
+                elif opcion == 3:
                     exit()
                 else:
                     print('INGRESE UN VALOR EN EL RANGO')
             break
 
 #MAIN
-pasajes = {000000: {'avion': 4523452, "clase": "primera", "asiento": "G8", "pasajero": 2354323}}
+pasajes = {000000: {'avion': 4523452, "clase": "primera", "asiento": "D4", "pasajero": 2354323}}
 vuelos = {000000: {"Fecha": "20/10/2024", "Origen": "Misiones", 'destino': 'eze'}}
 aviones = {"modelo": 'Boeing747'}
 
-matriz = crearMatriz(4,30,0)
+matrizEconomica = crearMatriz(6,26,0)
+matrizEjecutiva = crearMatriz(4,4,0)
 modificarPasaje(pasajes)
