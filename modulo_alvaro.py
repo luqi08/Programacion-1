fb4f227e42d597ddc5afde7ba87599a0e3ebafa1
Alvaro Beron <beronalvaro@gmail.com>
2024-10-08 16:55:59 -0300

    version mejorad de del pasajeros 2

--- modulo_alvaro.py
+++ modulo_alvaro.py
@@ -98,15 +98,20 @@ def eliminarPasajero(pasajeros):
         print("--------------------")
         opcion = input("Seleccione una opcion")
         if opcion == "1":
+            verificador = False
             print("Coloque el DNI del pasajero a eliminar")
             dni = int(input("DNI: "))
-            if dni in pasajeros == True:
+            if dni in list(pasajeros.keys()) == True:
                 verificador = True
-            while dni.isnumeric == False or verificador == False:
+            else:
+                verificador = False
+            while verificador == False:
                 print("Ese no es un dni posible")
                 dni = int(input("Intentelo nuevamente: "))
                 if dni in pasajeros == True:
                     verificador = True
+                else:
+                    verificador = False
             print(
                 "Aun esta seguro de que desea eliminar a dicho pasajero del diccionario?"
             )
