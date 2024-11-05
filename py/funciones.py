726dd699f169dbb755718b76fc673c877c73313e
Alvaro Beron <beronalvaro@gmail.com>
2024-11-05 16:20:38 -0300

    Funcion de registro de aviones agregada

--- py/funciones.py
+++ py/funciones.py
@@ -162,6 +162,7 @@ def letraNumero(letra: str) -> int:
         letra = 5
     return letra
 
+
 def mostrarMatrizdeVuelo(matriz: list[list], pasaje: dict) -> None:
     """
     Muestra visualmente la disposición de asientos en una matriz, con indicación de pasillo.
@@ -188,6 +189,7 @@ def mostrarMatrizdeVuelo(matriz: list[list], pasaje: dict) -> None:
             print("PASILLO".center(longitud * 3 + 1, "="))
     print("PUNTA DEL AVION A LA IZQUIERDA - COLA DEL AVION A LA DERECHA")
     return
+
 
 def mostrarMatriz(matriz: list[list], pasaje: dict, codigo: str) -> None:
     """
@@ -243,6 +245,7 @@ def mostrarMatrizCambiarClase(matriz: list[list], pasaje: dict, codigo: str) -> 
             print("PASILLO".center(longitud * 3 + 1, "="))
     print("PUNTA DEL AVION A LA IZQUIERDA - COLA DEL AVION A LA DERECHA")
     return
+
 
 def seleccionarAsiento(matriz):
     """
@@ -1194,7 +1197,7 @@ def registrarAviones(aviones: dict) -> int:
                 escribirModelo = input("Escribalo nuevamente: ")
             modelo = escribirModelo
         elif opcion == "2":
-            print("Escriba su apellido")
+            print("Escriba la matricula")
             escribirMatricula = input().upper()
             while escribirMatricula == "":
                 print("No es valido dejar el espacio en blanco")
@@ -1264,8 +1267,8 @@ def registrarAviones(aviones: dict) -> int:
     print("----------------")
     aviones[matricula] = {
         "modelo": modelo,
-        "asientos en primera clase": int(primeraClase),
-        "asientos en clase economica": int(claseEconomica),
+        "primera": int(primeraClase),
+        "economica": int(claseEconomica),
     }
     print("Avion registrado")
     return (porFilaPC, porColumnaPC, porFilaCE, porColumnaCE)
@@ -1286,3 +1289,96 @@ def eliminarAviones(aviones: dict):
     else:
         print("Esa opcion no existe")
         opcion = input("Intentelo nuevamente: ")
+
+
+def modificarAviones(aviones: dict):
+    print("Escriba la matricula del avion que desea modificar.")
+    matricula = input()
+    while matricula == "":
+        print("No se permiten los espacios en blanco")
+        matricula = input("Intentelo nuevamente: ")
+
+    modelo = aviones[matricula]["modelo"]
+    primeraClase = aviones["asientos"]["primera"]
+    claseEconomica = aviones["asientos"]["economica"]
+    verificador = False
+    while verificador == False:
+        print("Ingrese sus datos")
+        print("-----------------")
+        print(f"Modelo: {modelo}")
+        print(f"Matricula: {matricula}")
+        print(f"[3]Asientos de primera clase: {primeraClase}")
+        print(f"[4]Asientos de clase economica: {claseEconomica}")
+        print("[5]Realizar cambios (Reestablecera todo a su estado predeterminado)")
+        print("[6]Completar")
+        print("-----------------")
+        opcion = input("Seleccione una opcion: ")
+        if opcion == "3":
+            print("Escriba los asientos totales de la primera clase")
+            escribirPrimeraClase = input()
+            while escribirPrimeraClase == "":
+                print("No es valido dejar espacion en blanco ni involucrar letras")
+                escribirPrimeraClase = input("Intentelo nuevamente: ")
+            primeraClase = escribirPrimeraClase
+            print("Ahora coloque la cantidad de asientos por fila.")
+            porFilaPC = int(input())
+            print("Ahora coloque la cantidad de asientos por columna")
+            porColumnaPC = int(input())
+            while (porFilaPC * porColumnaPC) != primeraClase:
+                print("ERROR!")
+                print("La cantidad asientos debe coincidir con la dicha anteriormente.")
+                print("Coloque la cantidad de asientos por fila.")
+                porFilaPC = int(input())
+                print("Coloque la cantidad de asientos por columna")
+                porColumnaPC = int(input())
+        elif opcion == "4":
+            print("Escriba los asientos totales de la clase economica.")
+            escribirClaseEconomica = input()
+            while escribirClaseEconomica == "":
+                print("No es valido dejar espacios en blanco")
+                print("Intentelo nuevamente")
+                escribirClaseEconomica = input()
+            claseEconomica = escribirClaseEconomica
+            print("Ahora escriba los asientos por fila")
+            porFilaCE = int(input())
+            print("Ahora escriba la cantidad de asientos por columna")
+            porColumnaCE = int(input())
+            while (porFilaCE * porColumnaCE) == claseEconomica:
+                print(
+                    "Los datos otorgados son incorrectos y la cantidad de asientos por fila y columna debe coincidir"
+                )
+                print("Intentelo nuevamente")
+                print("Ahora escriba los asientos por fila")
+                porFilaCE = int(input())
+                print("Ahora escriba la cantidad de asientos por columna")
+                porColumnaCE = int(input())
+        elif opcion == "5":
+            modelo = ""
+            apellido = ""
+            dni = ""
+        elif opcion == "6":
+            if (
+                modelo == ""
+                or matricula == ""
+                or claseEconomica == ""
+                or primeraClase == ""
+            ):
+                print("Aun quedan datos por completar")
+            else:
+                break
+        else:
+            print("Esa opcion no existe")
+    print("Los datos")
+    print("----------------")
+    print(f"Modelo: {modelo}")
+    print(f"Matricula: {matricula}")
+    print(f"Asientos en primera clase: {primeraClase}")
+    print(f"Asientos en clase economica: {claseEconomica}")
+    print("----------------")
+    aviones[matricula] = {
+        "modelo": modelo,
+        "asientos en primera clase": int(primeraClase),
+        "asientos en clase economica": int(claseEconomica),
+    }
+    print("Avion registrado")
+    return (porFilaPC, porColumnaPC, porFilaCE, porColumnaCE)
